4324b3c9ed244732a58e211c3a7fa7388227abf5
ChiyunaZen <uni202406_11@telework.to>
2024-08-27 10:14:44 +0900

    データ管理系のクラスをcsファイルに分割

---
+++ RecipeApp/DateManagement.cs
@@ -0,0 +1,35 @@
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.IO;
+
+namespace RecipeApp
+{
+    public class DateManagement
+    {
+        public void SaveDate(List<Recipe> recipes)
+        {
+            var json = JsonConvert.SerializeObject(recipes, Newtonsoft.Json.Formatting.Indented);
+
+            //JSON文字列（json変数の値）をファイルに書き込む
+            File.WriteAllText(@"..\..\レシピリスト.json", json);
+
+        }
+
+        public List<Recipe> RoadDate()
+        {
+            List<Recipe> recipes = new List<Recipe>();
+
+            if (File.Exists(@"..\..\レシピリスト.json"))
+            {
+                //jsonファイル内のデータを読み込んで文字列型としてjson変数に代入
+                var json = File.ReadAllText(@"..\..\レシピリスト.json");
+                //読み取ったJSON文字列をRecipe型のリストrecipeにデシリアライズ
+                recipes = JsonConvert.DeserializeObject<List<Recipe>>(json);
+            }
+
+            return recipes;
+        }
+    }
+
+
+}
--- RecipeApp/Form1.cs
+++ RecipeApp/Form1.cs
@@ -1,10 +1,8 @@
-﻿using Newtonsoft.Json;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
-using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -22,8 +20,6 @@ namespace RecipeApp
         {
             InitializeComponent();
 
-            
-
             InitializeListView();
 
             dateManagement.SaveDate(recipes);
@@ -31,18 +27,10 @@ namespace RecipeApp
         }
 
 
-        private void InitializeListView() //リストボックスにリストの中身を表示するメソッド
+        private void InitializeListView() 
+            //リストボックスにリストの中身を表示するメソッド
         {
-
             recipes =  dateManagement.RoadDate();
-
-
-            ////仮でリスト登録してるだけです↓
-            //string[] takoyaki = { "小麦粉", "たこ", "紅ショウガ", "ネギ" };
-            //recipes.Add(new Recipe("たこやき", "30", takoyaki, "焼く"));
-            //string[] omurice = { "ごはん", "卵" };
-            //recipes.Add(new Recipe("オムライス", "30", omurice, "包む"));
-
 
             foreach (var recipe in recipes)
             {
@@ -50,9 +38,7 @@ namespace RecipeApp
                 listViewItem.SubItems.Add(recipe.CookingTime + "分");
 
                 recipeView.Items.Add(listViewItem);
-
             }
-
 
         }
 
@@ -62,33 +48,6 @@ namespace RecipeApp
             recipeView.Show();
         }
 
-    }
-
-    public class DateManagement
-    {
-        public void SaveDate(List<Recipe> recipes)
-        {
-            var json = JsonConvert.SerializeObject(recipes, Newtonsoft.Json.Formatting.Indented);
-
-            //JSON文字列（json変数の値）をファイルに書き込む
-            File.WriteAllText(@"..\..\レシピリスト.json", json);
-
-        }
-
-        public List<Recipe> RoadDate()
-        {
-            List<Recipe> recipes = new List<Recipe>();
-
-            if (File.Exists(@"..\..\レシピリスト.json"))
-            {
-                //jsonファイル内のデータを読み込んで文字列型としてjson変数に代入
-                var json = File.ReadAllText(@"..\..\レシピリスト.json");
-                //読み取ったJSON文字列をRecipe型のリストrecipeにデシリアライズ
-                recipes = JsonConvert.DeserializeObject<List<Recipe>>(json);
-            }
-
-            return recipes;
-        }
     }
 
 
