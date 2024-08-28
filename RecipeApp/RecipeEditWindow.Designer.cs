e7c9bc0abc779e61a49c2ffebf5c5c2733d6dd2e
ChiyunaZen <uni202406_11@telework.to>
2024-08-28 14:33:42 +0900

    微！

--- RecipeApp/RecipeEditWindow.Designer.cs
+++ RecipeApp/RecipeEditWindow.Designer.cs
@@ -44,23 +44,23 @@
             // 
             this.userControl_RecipeListView1.Location = new System.Drawing.Point(339, 12);
             this.userControl_RecipeListView1.Name = "userControl_RecipeListView1";
-            this.userControl_RecipeListView1.Size = new System.Drawing.Size(443, 252);
+            this.userControl_RecipeListView1.Size = new System.Drawing.Size(474, 252);
             this.userControl_RecipeListView1.TabIndex = 0;
             // 
             // AddButton
             // 
             this.AddButton.Location = new System.Drawing.Point(339, 284);
             this.AddButton.Name = "AddButton";
-            this.AddButton.Size = new System.Drawing.Size(276, 53);
+            this.AddButton.Size = new System.Drawing.Size(290, 53);
             this.AddButton.TabIndex = 1;
             this.AddButton.Text = "レシピの登録/編集";
             this.AddButton.UseVisualStyleBackColor = true;
             // 
             // RemoveButton
             // 
-            this.RemoveButton.Location = new System.Drawing.Point(627, 284);
+            this.RemoveButton.Location = new System.Drawing.Point(635, 284);
             this.RemoveButton.Name = "RemoveButton";
-            this.RemoveButton.Size = new System.Drawing.Size(161, 53);
+            this.RemoveButton.Size = new System.Drawing.Size(178, 53);
             this.RemoveButton.TabIndex = 2;
             this.RemoveButton.Text = "選択レシピの削除";
             this.RemoveButton.UseVisualStyleBackColor = true;
@@ -68,9 +68,9 @@
             // nameTextBox
             // 
             this.nameTextBox.Font = new System.Drawing.Font("MS UI Gothic", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
-            this.nameTextBox.Location = new System.Drawing.Point(67, 12);
+            this.nameTextBox.Location = new System.Drawing.Point(95, 12);
             this.nameTextBox.Name = "nameTextBox";
-            this.nameTextBox.Size = new System.Drawing.Size(162, 29);
+            this.nameTextBox.Size = new System.Drawing.Size(205, 29);
             this.nameTextBox.TabIndex = 3;
             // 
             // richTextBox1
@@ -83,9 +83,9 @@
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(67, 47);
+            this.textBox1.Location = new System.Drawing.Point(95, 47);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(130, 22);
+            this.textBox1.Size = new System.Drawing.Size(131, 22);
             this.textBox1.TabIndex = 5;
             // 
             // button1
@@ -107,16 +107,31 @@
             // 
             // numericUpDown1
             // 
-            this.numericUpDown1.Location = new System.Drawing.Point(258, 19);
+            this.numericUpDown1.Location = new System.Drawing.Point(256, 47);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            5,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
             this.numericUpDown1.Name = "numericUpDown1";
             this.numericUpDown1.Size = new System.Drawing.Size(44, 22);
             this.numericUpDown1.TabIndex = 8;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
             // 
             // RecipeEditWindow
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 354);
+            this.ClientSize = new System.Drawing.Size(825, 354);
             this.Controls.Add(this.numericUpDown1);
             this.Controls.Add(this.richTextBox2);
             this.Controls.Add(this.button1);
