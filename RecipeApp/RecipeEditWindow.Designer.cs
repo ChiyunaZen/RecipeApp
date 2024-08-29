a75fd871107deb6c6c006bcd9e2be26c4885ae44
ChiyunaZen <uni202406_11@telework.to>
2024-08-29 10:01:01 +0900

    レシピ編集画面のデザイン

--- RecipeApp/RecipeEditWindow.Designer.cs
+++ RecipeApp/RecipeEditWindow.Designer.cs
@@ -28,24 +28,21 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.userControl_RecipeListView1 = new RecipeApp.UserControl_RecipeListView();
             this.AddButton = new System.Windows.Forms.Button();
             this.RemoveButton = new System.Windows.Forms.Button();
             this.nameTextBox = new System.Windows.Forms.TextBox();
             this.richTextBox1 = new System.Windows.Forms.RichTextBox();
             this.textBox1 = new System.Windows.Forms.TextBox();
-            this.button1 = new System.Windows.Forms.Button();
             this.richTextBox2 = new System.Windows.Forms.RichTextBox();
-            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
-            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.userControl_RecipeListView1 = new RecipeApp.UserControl_RecipeListView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.levelStarLabel = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
             this.SuspendLayout();
-            // 
-            // userControl_RecipeListView1
-            // 
-            this.userControl_RecipeListView1.Location = new System.Drawing.Point(339, 12);
-            this.userControl_RecipeListView1.Name = "userControl_RecipeListView1";
-            this.userControl_RecipeListView1.Size = new System.Drawing.Size(474, 252);
-            this.userControl_RecipeListView1.TabIndex = 0;
             // 
             // AddButton
             // 
@@ -68,73 +65,110 @@
             // nameTextBox
             // 
             this.nameTextBox.Font = new System.Drawing.Font("MS UI Gothic", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
-            this.nameTextBox.Location = new System.Drawing.Point(95, 12);
+            this.nameTextBox.Location = new System.Drawing.Point(36, 32);
             this.nameTextBox.Name = "nameTextBox";
-            this.nameTextBox.Size = new System.Drawing.Size(205, 29);
+            this.nameTextBox.Size = new System.Drawing.Size(266, 29);
             this.nameTextBox.TabIndex = 3;
             // 
             // richTextBox1
             // 
-            this.richTextBox1.Location = new System.Drawing.Point(67, 126);
+            this.richTextBox1.Location = new System.Drawing.Point(36, 146);
             this.richTextBox1.Name = "richTextBox1";
-            this.richTextBox1.Size = new System.Drawing.Size(233, 68);
-            this.richTextBox1.TabIndex = 4;
+            this.richTextBox1.Size = new System.Drawing.Size(266, 58);
+            this.richTextBox1.TabIndex = 6;
             this.richTextBox1.Text = "";
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(95, 47);
+            this.textBox1.Location = new System.Drawing.Point(36, 88);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(131, 22);
+            this.textBox1.Size = new System.Drawing.Size(84, 22);
             this.textBox1.TabIndex = 5;
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(209, 314);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 6;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
             // 
             // richTextBox2
             // 
-            this.richTextBox2.Location = new System.Drawing.Point(67, 201);
+            this.richTextBox2.Location = new System.Drawing.Point(36, 231);
             this.richTextBox2.Name = "richTextBox2";
-            this.richTextBox2.Size = new System.Drawing.Size(233, 96);
+            this.richTextBox2.Size = new System.Drawing.Size(266, 106);
             this.richTextBox2.TabIndex = 7;
             this.richTextBox2.Text = "";
             // 
-            // numericUpDown1
+            // trackBar1
             // 
-            this.numericUpDown1.Location = new System.Drawing.Point(256, 47);
-            this.numericUpDown1.Maximum = new decimal(new int[] {
-            5,
-            0,
-            0,
-            0});
-            this.numericUpDown1.Minimum = new decimal(new int[] {
-            1,
-            0,
-            0,
-            0});
-            this.numericUpDown1.Name = "numericUpDown1";
-            this.numericUpDown1.Size = new System.Drawing.Size(44, 22);
-            this.numericUpDown1.TabIndex = 8;
-            this.numericUpDown1.Value = new decimal(new int[] {
-            1,
-            0,
-            0,
-            0});
+            this.trackBar1.Location = new System.Drawing.Point(186, 84);
+            this.trackBar1.Maximum = 5;
+            this.trackBar1.Minimum = 1;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(116, 56);
+            this.trackBar1.TabIndex = 5;
+            this.trackBar1.TickStyle = System.Windows.Forms.TickStyle.Both;
+            this.trackBar1.Value = 1;
+            // 
+            // userControl_RecipeListView1
+            // 
+            this.userControl_RecipeListView1.Location = new System.Drawing.Point(339, 12);
+            this.userControl_RecipeListView1.Name = "userControl_RecipeListView1";
+            this.userControl_RecipeListView1.Size = new System.Drawing.Size(474, 252);
+            this.userControl_RecipeListView1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("MS UI Gothic", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label1.Location = new System.Drawing.Point(126, 93);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(25, 17);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "分";
+            // 
+            // levelStarLabel
+            // 
+            this.levelStarLabel.AutoSize = true;
+            this.levelStarLabel.Location = new System.Drawing.Point(204, 125);
+            this.levelStarLabel.Name = "levelStarLabel";
+            this.levelStarLabel.Size = new System.Drawing.Size(82, 15);
+            this.levelStarLabel.TabIndex = 9;
+            this.levelStarLabel.Text = "★☆☆☆☆";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(36, 11);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(52, 15);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "料理名";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(36, 70);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(67, 15);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "調理時間";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(183, 70);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(52, 15);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "難易度";
             // 
             // RecipeEditWindow
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(825, 354);
-            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.levelStarLabel);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.trackBar1);
             this.Controls.Add(this.richTextBox2);
-            this.Controls.Add(this.button1);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.richTextBox1);
             this.Controls.Add(this.nameTextBox);
@@ -143,7 +177,7 @@
             this.Controls.Add(this.userControl_RecipeListView1);
             this.Name = "RecipeEditWindow";
             this.Text = "RecipeEditWindow";
-            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -157,8 +191,12 @@
         private System.Windows.Forms.TextBox nameTextBox;
         private System.Windows.Forms.RichTextBox richTextBox1;
         private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.Button button1;
         private System.Windows.Forms.RichTextBox richTextBox2;
-        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label levelStarLabel;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
     }
 }
